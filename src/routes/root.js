ff219e8709cc3f4934bbdeb2e094116442dc2aee
Eric Van Egmond <eric.vanegmond@pearson.com>
2019-07-23 13:50:25 -0700

    Removed error catch wraps

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,16 +1,15 @@
 const router = require('express').Router();
-const { wrap } = require('../utils/error');
 const {
   getRoutes,
   getHealth
 } = require('../handlers/root');
 
-router.get('/', wrap((req, res) => {
+router.get('/', (req, res) => {
   res.send(getRoutes());
-}));
+});
 
-router.get('/health', wrap((req, res) => {
+router.get('/health', (req, res) => {
   res.send(getHealth());
-}));
+});
 
 module.exports = router;
